050cb88c358575cdceebb9b990e7633be3532936
asmodis <asmodis@whiterabbit.(none)>
2008-11-18 12:26:38 +0100

    Added a LaTeX presenter

--- lib/smon/presenter.rb
+++ lib/smon/presenter.rb
@@ -3,16 +3,11 @@ require File.join(File.dirname(__FILE__), 'presenter', 'txt_presenter')
 class Presenter
   #Returns a LaTeX representation of the given object as a string.
   def self.to_tex(obj, opts = {})
-    puts "Not yet implemented"
+    TexPresenter.new.output obj, opts
   end
 
   #Returns an ASCII representation of the given object as a string.
   def self.to_txt(obj)    
     TxtPresenter.new.output obj
-  end
-
-  #Creates a pdf document in the tmp directory.
-  def self.to_pdf(obj, opts = {})
-    puts "Not yet implemented"
   end
 end
