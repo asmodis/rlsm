535523f47f5c298dadb727977671d2668d2bda95
asmodis <asmodis@whiterabbit.(none)>
2008-11-24 18:03:20 +0100

    Added a greeting message

--- lib/smon/smon.rb
+++ lib/smon/smon.rb
@@ -54,10 +54,12 @@ class Smon
     Categories['built-in'] = ['load']
 
     #Initialize the help system
-    @cmd_abbrev = Commands.abbrev + Columns.abbrev
+    @cmd_abbrev = (Commands + Columns).abbrev
 
     #Setting up readline
     Readline.completion_proc = lambda { |str| @cmd_abbrev[str] }
+
+    welcome_and_licence
 
     loop do
       begin
@@ -101,8 +103,54 @@ class Smon
     (Categories[cat] ||= []) << name
     Commands << name
     CmdHelp[name] = help
-    
+     
     @cmd_abbrev = Commands.abbrev if reinit
+  end
+
+  private
+  def welcome_and_licence
+    puts <<EOT
+(The MIT License)
+
+Copyright (c) 2008 Gunther Diemant <g.diemant@gmx.net>
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+ RRRRRRR      LL            SSSSSSSSS  MM                MM
+RR     RR     LL           SS          MMMM            MMMM
+RR      RR    LL          SS           MM MM          MM MM 
+RR      RR    LL          SS           MM  MM        MM  MM
+RR     RR     LL           SS          MM   MM      MM   MM
+RR    RR      LL            SSSSSSSS   MM    MM    MM    MM
+RRRRRRR       LL                   SS  MM     MM  MM     MM
+RR    RR      LL                    SS MM      MMMM      MM
+RR     RR     LL                    SS MM                MM
+RR      RR    LL                    SS MM                MM
+RR       RR   LL                   SS  MM                MM
+RR        RR  LLLLLLLLLLL SSSSSSSSSS   MM                MM
+
+Welcome to the rlsm interactive shell.
+Type help for an overview of the availible commands.
+
+EOT
   end
 end
 
