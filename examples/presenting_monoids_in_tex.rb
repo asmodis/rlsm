b350b671467daa1e3bf2247003f8db8170fe51f2
Gunther Diemant <asmodis@whiterabbit.(none)>
2009-10-05 16:09:08 +0200

    Finished example presenting_monoid

--- examples/presenting_monoids_in_tex.rb
+++ examples/presenting_monoids_in_tex.rb
@@ -1,34 +1,40 @@
 require File.join(File.dirname(__FILE__), '..', 'lib', 'rlsm')
+require 'enumerator'
+require 'tempfile'
 
 class Presenter
   @@template = <<TEMPLATE
+\\begin{minipage}{\\linewidth}
 \\begin{tabular}[t]{c|c}
 \\textbf{%l%table%%} & \\textbf{%l%dfa%%} \\\\ \\hline
-  %%table2tex%%  &  %%dfa2tex%%
-\\end{table}
+ %%table2tex%% &  %%dfa2tex%%
+\\end{tabular}\\\\[2ex]
 
 \\begin{tabular}{llcll}
-  \\multicolumn{2}{l}{\\textbf{Eigenschaften des Monoids:}} & & \\multicolumn{2}{l}{\\textbf{Spezielle Elemente:}} \\\\
+  \\multicolumn{2}{l}{\\textbf{%l%props%%:}} & & \\multicolumn{2}{l}{\\textbf{%l%special%%:}} \\\\
   %l%generator%%: & %%generating_subset%%   &                 & %l%idem_els%%: & %%idempotents%% \\\\
   %l%group%%:     & %%group?%%       &                        & %%zero1%% \\\\
   %l%comm%%:      & %%commutative?%% &                        & %%zero2%% \\\\
   %l%idem%%:      & %%idempotent?%%  &                        &                  & \\\\
-  %l%syn%%:       & %%syntactic?%%   &                        & \\multicolumn{2}{l}{\\\\textbf{%l%green%%:}} \\\\
+  %l%syn%%:       & %%syntactic?%%   &                        & \\multicolumn{2}{l}{\\textbf{%l%green%%:}} \\\\
   %l%aper%%:      & %%aperiodic?%%   &                        & %l%lclasses%%:   & %%l_classes%% \\\\
   %l%ltriv%%:     & %%l_trivial?%%   &                        & %l%rclasses%%:   & %%r_classes%% \\\\
   %l%rtriv%%:     & %%r_trivial?%%   &                        & %l%hclasses%%:   & %%h_classes%% \\\\
   %l%jtriv%%:     & %%j_trivial?%%   &                        & %l%dclasses%%:   & %%d_classes%% \\\\
   %l%has_zero%%:  & %%zero?%%        &                        &                  &               \\\\
-\\end{tabular}
+\\end{tabular}\\\\[1ex]
 
 \\textbf{%l%submons%%:} %%submonoids%%
 
 %%syntactic_properties%%
+\\end{minipage}\\\\[2ex]
 TEMPLATE
 
   @@lang = { 
     :en => Hash['table', 'Binary Operation', 
                 'dfa', 'DFA',
+                'props', 'Properties of the monoid',
+                'special','Special elements',
                 'generator', 'Generator',
                 'group', 'Group',
                 'comm', 'Commutative',
@@ -38,7 +44,7 @@ TEMPLATE
                 'ltriv', 'L-trivial',
                 'rtriv', 'R-trivial',
                 'jtriv', 'J-trivial',
-                'has_zero', 'Has zero elemnt',
+                'has_zero', 'Has zero element',
                 'idem_els', 'Idempotents',
                 'zero', 'Zero element',
                 'lzero', 'Left zeros',
@@ -50,10 +56,15 @@ TEMPLATE
                 'dclasses', 'D-classes',
                 'submons', 'Submonoids',
                 'none', 'none',
+                'synprop','Syntactic Properties',
+                'disj', 'Disjunctive Subset',
+                'reg', 'Regular Expression',
                 true, 'yes',
                 false, 'no'], 
     :de => Hash['table', 'Binäre Operation',
                 'dfa', 'DFA',
+                'props', 'Eigenschaften des Monoids',
+                'special','Spezielle Elemente',
                 'generator', 'Erzeuger',
                 'group', 'Gruppe',
                 'comm', 'Kommutativ',
@@ -75,14 +86,19 @@ TEMPLATE
                 'dclasses', 'D-Klassen',
                 'submons', 'Untermonoide',
                 'none', 'keine',
+                'synprop','Syntaktische Eigenschaften',
+                'disj', 'Disjunktive Teilmenge',
+                'reg', 'Regulärer Ausdruck',
                 true, 'ja',
                 false, 'nein'] 
   }
 
-  def self.to_latex(monoid = RLSM::Monoid['012 120 201'], lang = :en)
-    presenter = Presenter.new(monoid, lang)
+  def self.to_latex(options = {})
+    options[:monoid] ||= RLSM::Monoid['012 120 201']
+    options[:lang] ||= :en
+    presenter = Presenter.new(options[:monoid], options[:lang])
     output = @@template.dup
-    @@lang[lang].each_pair do |key, text|
+    @@lang[options[:lang]].each_pair do |key, text|
       output.sub!("%l%#{key}%%", text)
     end
 
@@ -90,7 +106,7 @@ TEMPLATE
       output.sub!($~[0], presenter.send($~[1].to_sym))
     end
 
-    puts output
+    output
   end
 
   def initialize(monoid,lang)
@@ -99,28 +115,110 @@ TEMPLATE
   end
 
   def table2tex
-    @monoid.to_s
+    helper_table2tex(@monoid)
   end
 
-  def dfa2tex
-    "not implemented"
+
+  def dfa2tex(opts = { :dot => true })
+    dfa = @monoid.to_dfa
+    if opts[:dot] and not %x(which dot).empty?
+      filename = tmp_filename
+      dfa2pic dfa, :filename => filename, :format => 'plain'
+
+      str = "\\begin{xy}\n0;<1.27cm,0cm>:\n"
+      
+      edges = []
+      File.open(filename + ".plain", 'r').each_line do |line|
+          values = line.split
+        if ['edge','node'].include? values.first
+          str += tex_xy_node(values)
+        end
+        edges << tex_xy_edge(values) if values.first == 'edge'
+      end
+      
+      str += edges.join("\n")
+      
+      File.delete(filename + ".plain")
+      
+      return str + "\n\\end{xy}\n"
+    else
+      str = "\\begin{tabular}{r|" +
+        (['c']*dfa.alphabet.size).join('|') + "}\n"
+      str += " & " + dfa.alphabet.map do |l|
+        "\\textbf{#{l}}"
+      end.join(' & ') + " \\\\ \\hline\n"
+      dfa.states.each do |state|
+        tmp_str = ''
+        tmp_str += "\\ensuremath{*}" if dfa.finals.include? state
+        tmp_str += "\\ensuremath{\\rightarrow}" if dfa.initial_state == state
+        tmp_str += state + " & "
+        tmp_str += dfa.alphabet.map do |letter|
+          tmp = dfa[letter,state]
+          tmp ? tmp : 'nil'
+        end.join(' & ')
+        str+= tmp_str + " \\\\\n"
+      end
+      
+      return str + "\\end{tabular}\n"
+    end
+  end
+  
+  def submonoids
+    submons = @monoid.proper_submonoids.map { |sm| helper_table2tex(sm) }
+
+    if submons.empty?
+      @lang['none'] + "\\\\\\\\[2ex]"
+    else
+      submons.join("\\quad") + "\\\\\\\\[2ex]"
+    end
   end
 
   def zero1
-    'foo'
+    if @monoid.zero?
+      "#{@lang['zero']}: & #{@monoid.zero} \\\\"
+    else
+      unless @monoid.left_zeros.empty?
+        "#{@lang['lzero']}: & #{@monoid.left_zeros.join(', ')} \\\\"
+      else
+        "  &   \\\\"
+      end
+    end
   end
 
   def zero2
-    'bar'
+    if @monoid.zero?
+      "  &   \\\\"
+    else
+      unless @monoid.right_zeros.empty?
+        "#{@lang['rzero']}: & #{@monoid.right_zeros.join(', ')} \\\\"
+      else
+        "  &   \\\\"
+      end
+    end
   end
 
   def syntactic_properties
-    'baz'
+    if @monoid.syntactic?
+      table_text = ""
+      @monoid.all_disjunctive_subsets.each do |set|
+        table_text += set2tex(set) + "& " + reg2tex(@monoid.to_dfa(set).to_regexp) + "\\\\\\\\"
+      end
+
+      output = <<TEXT
+\\textbf{#{@lang['synprop']}:}
+\\begin{center}
+\\begin{tabular}{cl}
+\\textbf{#{@lang['disj']}} & \\textbf{#{@lang['reg']}} \\\\\\\\ \\hline
+#{table_text}
+\\end{tabular}
+\\end{center}
+TEXT
+    else
+      ""
+    end
   end
 
   def method_missing(name, *args)
-p name
-p @monoid
     if name.to_s =~ /\?/
       @lang[@monoid.send(name)]
     else
@@ -128,7 +226,113 @@ p @monoid
     end
   end
 
+  private
   def set2tex(set)
-    "\{" + set.join(', ') + "\}"
+    if Array === set.first
+      set.map! { |s| set2tex(s) }.join(', ')
+    else
+      "\\{" + set.join(', ') + "\\}"
+    end
+  end
+
+  def helper_table2tex(monoid)
+    table = []
+    monoid.binary_operation.table.each_with_index do |x, i| 
+      table << [] if i % monoid.order == 0
+      table.last << @monoid.binary_operation.elements[x]
+    end
+
+    buffer = ['\begin{tabular}[t]{' +
+               (['c']*(monoid.order+1)).join('|') + '|}']
+ 
+    buffer << ' & ' + table[0].map do |el|
+      "\\textbf{#{el}}"
+    end.join(' & ') + "\\\\\\\\ \\hline"
+ 
+    table.each do |row|
+      buffer << "\\textbf{#{row[0]}} & " + row.join(' & ') + "\\\\\\\\ \\hline"
+    end
+ 
+    buffer << ['\end{tabular}']
+    buffer.join("\n")
+  end
+
+  def reg2tex(re)
+    re_str = re.string.
+      gsub(RLSM::RE::ParserHelpers::EmptyWordSymbol, "\\lambda ").
+      gsub(RLSM::RE::ParserHelpers::UnionSymbol, "\\mid ").
+      gsub(RLSM::RE::ParserHelpers::StarSymbol, "^{*}")
+    "\\ensuremath{#{re_str}}"
+  end
+
+  def tex_xy_node(n)
+    if n.first == 'edge'
+      return "" if n[1] == 'preinit'
+ 
+      num_points = n[3].to_i
+      lable_pos = n[2*num_points + 5,2].join(',')
+      label = n[2*num_points + 4].gsub("\"",'')
+ 
+      return ";(#{lable_pos})*[r]\\txt{#{label}}\n"
+    else
+      id = n[1]
+      xypos = n[2,2].join(',')
+      return "(#{xypos})*{}=\"#{id}\"\n" if n[1] == 'preinit'
+ 
+      label = n[6]
+      type = n[8] == 'circle' ? '' : '='
+ 
+      return ";(#{xypos})*+=[o]++[F#{type}]{#{label}}=\"#{id}\"\n"
+    end
+  end
+ 
+  def tex_xy_edge(e)
+    from, to = e[1,2]
+    num_points = e[3].to_i
+    points = points_to_str(e[4,2*num_points])
+ 
+    "\\ar @`{#{points}} \"#{from}\";\"#{to}\""
+  end
+ 
+  def points_to_str(a)
+    "(" + a.enum_slice(2).to_a.map { |x| x.join(',') }.join('),(') + ")"
+  end
+
+  def tmp_filename
+    File.join(Dir.tmpdir, "m#{@monoid.to_s.gsub(/\s*,?/, '_')}")
+  end
+
+  def dfa2pic(dfa, options = {:format => 'png'})
+    filename = options[:filename] || tmp_filename
+    File.open(filename + ".dot", "w") { |f| f.puts dfa2dot(dfa) }
+    system "dot -T#{options[:format]} -o #{filename}.#{options[:format]} #{filename}.dot"
+    File.delete(filename + ".dot")
+  end
+ 
+  #Creates string which is a dot representation of a DFA.
+  def dfa2dot(dfa)
+    str = "digraph {\n"
+    str += "size=\"2,2\"\nratio=1.0\n"
+    str += "node [shape=circle]\n"
+    str += "preinit [shape=plaintext, label=\"\"]\n"
+    (dfa.states - dfa.final_states).each do |state|
+      str += state + "\n"
+    end
+    str += "node [shape=doublecircle]\n"
+    dfa.final_states.each do |state|
+      str += state + "\n"
+    end
+    str += "preinit -> #{dfa.initial_state}\n"
+    dfa.states.each do |s1|
+      dfa.states.each do |s2|
+        res = dfa.transitions.find_all { |tr| tr[0] == s1 and tr[1] == s2 }
+        unless res.empty?
+          label = res.map { |tr| tr[2] }.join(',')
+          str += s1 + "->" + s2 + "[label=\"#{label}\"]\n"
+        end
+      end
+    end
+ 
+    str + "}"
   end
 end
