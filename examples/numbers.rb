1802030524434e83d5af537130aea33da3e6a642
Gunther Diemant <asmodis@whiterabbit.localdomain>
2010-07-08 20:47:13 +0200

    Added an example

---
+++ examples/numbers.rb
@@ -0,0 +1,41 @@
+#-*- ruby -*-
+
+require File.join(File.dirname(__FILE__), '..', 'lib', 'rlsm')
+require "rubygems"
+require "sqlite3"
+
+$db = SQLite3::Database.new("../data/monoids.db")
+
+[2,3,4,5,6].each do |i|
+  puts "Monoids of order #{i}:"
+  total = 0
+  syntactic = 0
+  regular = 0
+  reg_ns = 0
+  inverse = 0
+  inv_ns = 0
+  
+  $db.execute("SELECT binop FROM monoids WHERE ord=#{i};").each do |binop|
+    monoid = RLSM::Monoid[*binop]
+
+    total += 1
+    if monoid.syntactic?
+      syntactic += 1
+      regular += 1 if monoid.regular?
+      inverse += 1 if monoid.inverse?
+    else
+      regular += 1; reg_ns += 1 if monoid.regular?
+      inverse += 1; inv_ns += 1 if monoid.inverse?
+    end
+  end
+
+  puts "        Total: #{total}"
+  puts "    Syntactic: #{syntactic}"
+  puts "Not Syntactic: #{total - syntactic}"
+  puts "      Regular: #{regular}"
+  puts "      Inverse: #{inverse}"
+  puts "   Regular ns: #{reg_ns}"
+  puts "   Inverse ns: #{inv_ns}"
+  puts
+  puts
+end
