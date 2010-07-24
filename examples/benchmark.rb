4b6080057dde00b2864166b028593eade75d29cb
Gunther Diemant <g.diemant@gmx.net>
2010-07-24 13:07:30 +0200

    Benchmark updated.

--- examples/benchmark.rb
+++ examples/benchmark.rb
@@ -3,8 +3,12 @@ require File.join(File.dirname(__FILE__), '..', 'lib', 'rlsm')
 #f = File.open("tempfile.txt",'w')
 x=0
 start = Time.now
-2.times { RLSM::Monoid.each_table(6) { x += 1 } }
+RLSM::Monoid.each_table(6) { x += 1 }
 puts "Elapsed time: #{Time.now - start}"
+start = Time.now
+RLSM::Monoid.each(6) { x += 1 }
+puts "Elapsed time: #{Time.now - start}"
+
 puts
 
 #f.close
